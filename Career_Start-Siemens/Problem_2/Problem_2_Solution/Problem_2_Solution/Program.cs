c51f67879983ec018403df8869b530dd5e7fcaff
Alex <58958500+SpaceWK@users.noreply.github.com>
2022-11-12 22:19:11 +0200

    Resolv the problem. Parse, sort, remove duplicates and display the array.

--- Career_Start-Siemens/Problem_2/Problem_2_Solution/Problem_2_Solution/Program.cs
+++ Career_Start-Siemens/Problem_2/Problem_2_Solution/Problem_2_Solution/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Specialized;
+using System.Reflection.Metadata;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
 
 namespace Problem_2_Solution
 {
@@ -15,11 +18,13 @@ namespace Problem_2_Solution
 
             var random = new Random();
 
-            List<string> stringsList = new List<string>();
+            List<string> stringsList = new();
             string[] stringsArray = new string[numberOfStrings];
 
-            Console.Write("\n" + "The String is: " + "\n");
-            for (int j = 0; j < numberOfStrings; j++) {
+            Console.Write("\n" + "The strings are: " + "\n");
+
+            while (numberOfStrings > 0) {
+
                 int randomLenght = random.Next(10, 100);
                 var stringChars = new char[randomLenght];
 
@@ -35,38 +40,114 @@ namespace Problem_2_Solution
                 }
 
                 var finalString = new String(stringChars);
-                Console.WriteLine(finalString);
+
+                if (numberOfStrings != 1) {
+                    Console.WriteLine("'" + finalString + "',");
+                } else {
+                    Console.WriteLine("'" + finalString + "'");
+                }
+
                 stringsList.Add(finalString);
+                numberOfStrings--;
             }
             
             stringsArray = stringsList.ToArray();
             return stringsArray;
         }
 
-        public static string[] ParseTheArrayOfStrings(string[] arrayOfStrings) {
+        public static int[] ParseTheArrayOfStrings(string[] arrayOfStrings) {
 
+            int[] arrayOfNumbers;
+            List<int> numberList = new();
+            Queue<int> q = new();
 
-            return null;
+            Console.Write("\n" + "The numbers are: " + "\n");
+
+            foreach (string str in arrayOfStrings) {
+
+                Regex regex = new Regex(@"[ ](?=(?:[^""]*""[^""]*"")*[^""]*$)", RegexOptions.Multiline);
+                string[] splits = regex.Split(str);
+
+                foreach (string split in splits) {
+                    
+                    string numbersPerSplit = string.Empty;
+                    int val;
+                    for (int i = 0; i < split.Length; i++) {
+                        if (Char.IsDigit(split[i]))
+                            numbersPerSplit += split[i];
+                    }
+                    if (numbersPerSplit.Length > 0) {
+                        val = int.Parse(numbersPerSplit);
+                        numberList.Add(val);
+                        q.Enqueue(val);
+                    }
+                }
+
+                Console.Write("[");
+                while (q.Count > 0) {
+                    if (q.Count != 1) {
+                        Console.Write(q.Dequeue() + ",");
+                    } else {
+                        Console.Write(q.Dequeue());
+                    }
+                }
+                Console.Write("]");
+
+                q = new();
+            }
+
+            arrayOfNumbers = numberList.ToArray();
+            return arrayOfNumbers;
         }
 
-        public static string SortTheArrayOfStrings(string[] arrayOfNumbers) {
+        public static int[] RemoveDuplicatesFromSortedArray(int[] arrayOfNumbers) {
 
+            int index = 1;
+            for (int i = 0; i < arrayOfNumbers.Length - 1; i++) {
+                if (arrayOfNumbers[i] != arrayOfNumbers[i + 1]) {
+                    arrayOfNumbers[index] = arrayOfNumbers[i + 1];
+                    index++;
+                }
+                else {
+                    continue;
+                }
+            }
 
-            return null;
+            int[] newarr = new int[index];
+            for (int i = 0; i < index; i++) {
+                newarr[i] = arrayOfNumbers[i];
+            }
+            return newarr;
+        }
+
+        public static void SortTheArrayOfStrings(int[] arrayOfNumbers) {
+  
+            Array.Sort(arrayOfNumbers);
+
+            int lastValue = arrayOfNumbers.Last();
+
+            int[] finalArrayOfNumbers = RemoveDuplicatesFromSortedArray(arrayOfNumbers);
+
+            Console.Write("\n" + "\n"  + "The Output is: " + "\n" + "[");
+            foreach (int i in finalArrayOfNumbers) {
+                if (i != lastValue) {
+                    Console.Write(i + ",");
+                } else {
+                    Console.Write(i);
+                }
+            }
+            Console.Write("]" + "\n");
         }
 
         static void Main(string[] args) {
 
             Console.Write("Give a number of strings: ");
-            int numberOfStrings = Int32.Parse(Console.ReadLine());
+            int numberOfStrings = int.Parse(Console.ReadLine());
 
             string[] arrayOfString = CreateRandomArrayOfStrings(numberOfStrings);
-            //string[] arrayOfNumbers = ParseTheArrayOfStrings(arrayOfString);
-            //foreach (string str in arrayOfNumbers) {
-            //    Console.Write("The Output is: " + "[" + str + "],");
-            //}
-            //string finalString = SortTheArrayOfStrings(arrayOfNumbers);
-            //Console.Write("The Output is: " + "["+ finalString + "]");
+            //string[] arrayOfString = {"12 might 45 % internship 2022" , "array of 5 elements" , " best fo[23]%6c abc 45 "};
+            int[] arrayOfNumbers = ParseTheArrayOfStrings(arrayOfString);
+            SortTheArrayOfStrings(arrayOfNumbers);
         }
     }
 }
